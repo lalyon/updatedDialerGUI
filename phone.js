ec4341f2d6a715a61025c31790d6c7e7040310e3
Lucas Lyon <lucas.a.lyon@gmail.com>
2019-02-12 13:26:45 -0700

    adding all files to GitHub Pages

--- phone.js
+++ phone.js
@@ -2,6 +2,7 @@ $(document).ready(function() { // do this when the document is loaded
   $("#content_dialer").show();
 	$("#content_contacts").hide();
 	$("#content_add").hide();
+  $("#content_help").hide();
 });
 
 
@@ -9,18 +10,30 @@ $("#dialer_tab_btn").click(function() { // when "dialer_tab" is clicked
   $("#content_dialer").show();
   $("#content_contacts").hide();
   $("#content_add").hide();
+  $("#content_help").hide();
+
 });
 
-$("#contacts_tab_btn").click(function() { // when "dialer_tab" is clicked
+$("#contacts_tab_btn").click(function() { // when "contacts_tab" is clicked
   $("#content_dialer").hide();
   $("#content_contacts").show();
   $("#content_add").hide();
+  $("#content_help").hide();
+
 });
 
-$("#add_tab_btn").click(function() { // when "dialer_tab" is clicked
+$("#add_tab_btn").click(function() { // when "add_tab" is clicked
   $("#content_dialer").hide();
   $("#content_contacts").hide();
   $("#content_add").show();
+  $("#content_help").hide();
+});
+
+$("#help_tab_btn").click(function() { // when "help_tab" is clicked
+  $("#content_dialer").hide();
+  $("#content_contacts").hide();
+  $("#content_add").hide();
+  $("#content_help").show();
 });
 
 $(".numpad_btn").click(function() {
